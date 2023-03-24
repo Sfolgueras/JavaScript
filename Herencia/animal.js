860d66ade166aa4eea8d3258e83588789d0d37ed
Sfolgueras <sfolgueras@nigul.cide.es>
2023-03-24 23:26:10 +0100

    herencia

---
+++ Herencia/animal.js
@@ -0,0 +1,25 @@
+class Animal{
+    constructor(especie, edad, color){
+        this.especie = especie;
+        this.edad = edad;
+        this.color = color;
+        this.informacion = `Soy ${this.especie}, tengo ${this.edad} años y soy de color ${this.color}`;
+    }
+    verInfo = ()=>{
+        document.write(this.informacion+ "<br>")
+     }
+}
+
+class Perro extends Animal{
+    constructor(especie, edad, color ,peso){
+        super (especie, edad, color);
+        this.peso = peso;
+    }
+    cagar = ()=>{
+        alert("Me cague")
+    }
+}
+
+let firulais = new Perro("bicho",6,"marron", 45);
+firulais.cagar();
+firulais.verInfo();
